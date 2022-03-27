014c981e21ca3f6afcaeea9feebee7165e24b297
Harshit <harshitsaini3582@gmail.com>
2022-03-27 13:40:53 +0530

    Added solution - LeetHub

---
+++ Largest square formed in a matrix - GFG/largest-square-formed-in-a-matrix.java
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String a[] = in.readLine().trim().split("\\s+");
+            int n = Integer.parseInt(a[0]);
+            int m = Integer.parseInt(a[1]);
+            String a1[] = in.readLine().trim().split("\\s+");
+            int mat[][] = new int[n][m];
+            for(int i = 0;i < n*m;i++)
+                mat[i/m][i%m] = Integer.parseInt(a1[i]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.maxSquare(n, m, mat));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int maxSquare(int n, int m, int matrix[][]){
+        // code here
+         int aux[][]= new int[n+1][m+1];
+      int max=0;
+      for(int i=1; i<=n; i++){
+          for(int j=1; j<=m; j++){
+              if (aux[i][j]==0 && matrix[i-1][j-1]==1) aux[i][j]=1;
+              if(matrix[i-1][j-1]==1){
+                  aux[i][j]= Math.min(aux[i-1][j], Math.min(aux[i][j-1],aux[i-1][j-1]))+1;
+              }else{
+                  aux[i][j]=0;
+              }
+              max=Math.max(max,aux[i][j]);
+          }
+      }
+      return max;
+    }
+}
