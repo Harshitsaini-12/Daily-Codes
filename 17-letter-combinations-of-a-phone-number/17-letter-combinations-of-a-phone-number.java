f416878465aa2453a07862d2c27d6a71594ddceb
Harshit <harshitsaini3582@gmail.com>
2022-03-29 09:33:08 +0530

    Time: 9 ms (21.73%), Space: 44.2 MB (7.26%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,45 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> list = new ArrayList<>();
+        if(digits.length()==0){
+            return list;
+        }
+        
+        List<String>ans=new ArrayList<>();
+        getKPC(digits,0,ans,"");
+        return ans;
+        
+    }
+    
+    static String[] codes={
+         "",
+        "",
+        "abc",
+        "def",
+        "ghi",
+        "jkl",
+        "mno",
+        "pqrs",
+        "tuv",
+       "wxyz"
+    };
+    
+    private static int getKPC(String digits,int idx,List<String>ans,String psf){
+        if(idx==digits.length()){
+            ans.add(psf);
+            return 1;
+        }
+        
+        
+        int count=0;
+        int val=digits.charAt(idx)-'0';
+        String code=codes[val];
+        
+        
+        for(int i=0;i<code.length();i++){
+            count+=getKPC(digits,idx+1,ans,psf+code.charAt(i));
+        }
+        
+        return count;
+    }
+}
