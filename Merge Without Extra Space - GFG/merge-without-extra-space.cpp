fcd162e5285a3d934e06d1bb2fbf6e774868a464
Harshit <harshitsaini3582@gmail.com>
2022-04-21 14:26:27 +0530

    Added solution - LeetHub

--- Merge Without Extra Space - GFG/merge-without-extra-space.cpp
+++ Merge Without Extra Space - GFG/merge-without-extra-space.cpp
@@ -7,30 +7,34 @@ using namespace std;
 class Solution{
     public:
         //Function to merge the arrays.
-        void merge(long long ar1[], long long ar2[], int n, int m) 
+        void merge(long long arr1[], long long arr2[], int n, int m) 
         { 
-            // code here 
-            int gap = ceil((float)(n + m) / 2);
-  while (gap > 0) {
-    int i = 0;
-    int j = gap;
-    while (j < (n + m)) {
-      if (j < n && ar1[i] > ar1[j]) {
-        swap(ar1[i], ar1[j]);
-      } else if (j >= n && i < n && ar1[i] > ar2[j - n]) {
-        swap(ar1[i], ar2[j - n]);
-      } else if (j >= n && i >= n && ar2[i - n] > ar2[j - n]) {
-        swap(ar2[i - n], ar2[j - n]);
-      }
-      j++;
-      i++;
-    }
-    if (gap == 1) {
-      gap = 0;
-    } else {
-      gap = ceil((float) gap / 2);
-    }
-  }
+            // code here
+            int gap=ceil(float(n+m)/2);
+            
+            while(gap>0){
+                int i=0;
+                int j=gap;
+                
+                while(j<(n+m)){
+                    if(j<n && arr1[i]>arr1[j]){
+                        swap(arr1[i],arr1[j]);
+                    }else if(j>=n && i<n && arr1[i]>arr2[j-n]){
+                        swap(arr1[i],arr2[j-n]);
+                    }else if(j >= n && i >= n && arr2[i - n] > arr2[j - n]){
+                        swap(arr2[i - n], arr2[j - n]);
+                    }
+                    
+                    i++;
+                    j++;
+                }
+                
+                if(gap==1){
+                    gap=0;
+                }else{
+                    gap=ceil((float)gap/2);
+                }
+            }
         } 
 };
 
