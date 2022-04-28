e68ecd2726ce2f4ae87081e33b8df1ca3ab5b9fc
Harshit <harshitsaini3582@gmail.com>
2022-04-28 17:35:43 +0530

    Time: 1594 ms (5.01%), Space: 57.8 MB (13.51%) - LeetHub

---
+++ 322-coin-change/322-coin-change.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int x=ans(coins,0,amount,new HashMap<String,Integer>());
+        
+        if(x==100000)return -1;
+        
+        return x;
+    }
+    
+    public int ans(int[] coins, int index,int amount,HashMap<String,Integer>hm){
+        if(amount==0)return 0;
+        
+        if(index>=coins.length){
+            return 100000;
+        }
+        
+        int consider=100000;
+        
+        String s=Integer.toString(index)+"#"+Integer.toString(amount);
+        
+        if(hm.containsKey(s)){
+            return hm.get(s);
+        }
+        
+        if(coins[index]<=amount){
+            consider=1+ans(coins,index,amount-coins[index],hm);
+        }
+        
+        int notConsider=ans(coins,index+1,amount,hm);
+        
+        hm.put(s,Math.min(consider,notConsider));
+        
+        return Math.min(consider,notConsider);
+    }
+}
