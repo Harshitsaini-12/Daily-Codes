f17b3d9168b01c70a7c55909b7a4f23b9d9118c5
Harshit <harshitsaini3582@gmail.com>
2022-05-12 10:07:40 +0530

    Time: 5 ms (86.54%), Space: 45 MB (19.09%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
+//     public int lengthOfLIS(int[] nums) {
 //         int ans=1;
 //         int n=nums.length;
         
@@ -15,32 +15,70 @@ class Solution {
 //             }
 //         }
 //         return ans;
-        
-        int n=nums.length;
-        int []dp=new int[n];
-        int maxlen=0;
-        
-        for(int i=0;i<n;i++){
-            maxlen=Math.max(maxlen,lis(nums,i,dp));
-        }
-        
-        return maxlen;
-    }
+
+//     }
     
-    private int lis(int[]nums,int idx,int[]dp){
-        int maxlen=1;
+    // 0(nlogn)
+    
+    public int binary_Search(int val,List<Integer>list){
+        int li=0,ri=list.size();
         
-        if(dp[idx]!=0){
-            return dp[idx];
-        }
-        
-        for(int i=idx-1;i>=0;i--){
-            if(nums[idx]>nums[i]){
-                int ans=lis(nums,i,dp);
-                maxlen=Math.max(maxlen,ans+1);
+        while(li<ri){
+            int mid=(li+ri)/2;
+            
+            if(val<=list.get(mid)){
+                ri=mid;
+            }else{
+                li=mid+1;
             }
         }
         
-        return dp[idx]=maxlen;
+        return li;
+    }
+    
+    
+    public int lengthOfLIS(int[] nums) {
+        
+        int n=nums.length;
+        List<Integer>list=new ArrayList<>();
+        
+        for(int i=0;i<n;i++){
+            int idx=binary_Search(nums[i],list);
+            
+            if(idx==list.size())list.add(nums[i]);
+            else{
+                list.set(idx,nums[i]);
+            }
+            
+        }
+        return list.size();
     }
 }
+
+        
+//         int n=nums.length;
+//         int []dp=new int[n];
+//         int maxlen=0;
+        
+//         for(int i=0;i<n;i++){
+//             maxlen=Math.max(maxlen,lis(nums,i,dp));
+//         }
+        
+//         return maxlen;
+    // }
+    
+//     private int lis(int[]nums,int idx,int[]dp){
+//         int maxlen=1;
+        
+//         if(dp[idx]!=0){
+//             return dp[idx];
+//         }
+        
+//         for(int i=idx-1;i>=0;i--){
+//             if(nums[idx]>nums[i]){
+//                 int ans=lis(nums,i,dp);
+//                 maxlen=Math.max(maxlen,ans+1);
+//             }
+//         }
+        
+//         return dp[idx]=maxlen;
