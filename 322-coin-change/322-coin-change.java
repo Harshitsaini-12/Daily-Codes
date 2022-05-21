306e6766af026e680c7f16b0c2fee85964cee449
Harshit <harshitsaini3582@gmail.com>
2022-05-21 10:28:18 +0530

    Time: 27 ms (49.53%), Space: 44.8 MB (64.89%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,35 +1,46 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int x=ans(coins,0,amount,new HashMap<String,Integer>());
+//         int x=ans(coins,0,amount,new HashMap<String,Integer>());
         
-        if(x==100000)return -1;
+//         if(x==100000)return -1;
         
-        return x;
-    }
+//         return x;
+//     }
     
-    public int ans(int[] coins, int index,int amount,HashMap<String,Integer>hm){
-        if(amount==0)return 0;
+//     public int ans(int[] coins, int index,int amount,HashMap<String,Integer>hm){
+//        if(amount==0)return 0;
         
-        if(index>=coins.length){
-            return 100000;
+//         if(index>=coins.length){
+//             return 100000;
+//         }
+        
+//         int consider=100000;
+        
+//         String s=Integer.toString(index)+"#"+Integer.toString(amount);
+        
+//         if(hm.containsKey(s)){
+//             return hm.get(s);
+//         }
+        
+//         if(coins[index]<=amount){
+//             consider=1+ans(coins,index,amount-coins[index],hm);
+//         }
+        
+//         int notConsider=ans(coins,index+1,amount,hm);
+        
+//         hm.put(s,Math.min(consider,notConsider));
+        
+//         return Math.min(consider,notConsider);
+//     }
+   int[] dp = new int[amount + 1];
+        for (int target = 1; target < dp.length; target++) {
+            dp[target] = dp.length;
+            for (int coin : coins) {
+                if (target >= coin) {
+                    dp[target] = Math.min(dp[target], dp[target - coin] + 1);
+                }
+            }
         }
-        
-        int consider=100000;
-        
-        String s=Integer.toString(index)+"#"+Integer.toString(amount);
-        
-        if(hm.containsKey(s)){
-            return hm.get(s);
-        }
-        
-        if(coins[index]<=amount){
-            consider=1+ans(coins,index,amount-coins[index],hm);
-        }
-        
-        int notConsider=ans(coins,index+1,amount,hm);
-        
-        hm.put(s,Math.min(consider,notConsider));
-        
-        return Math.min(consider,notConsider);
-    }
+        return dp[amount] == dp.length ? -1 : dp[amount];
+}
 }
