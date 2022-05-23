93dd7b2b6a9535649b89f3ef8100c71f968ded03
Harshit <harshitsaini3582@gmail.com>
2022-05-23 11:07:36 +0530

    Time: 14 ms (62.39%), Space: 42.9 MB (60.24%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
@@ -8,45 +8,46 @@
  * }
  */
 class Solution {
-    public void kdown(TreeNode node,int k,TreeNode blocker,List<Integer>ans){
-            if(node==null || k<0 ||node==blocker)return;
-
-            if(k==0){
-             ans.add(node.val);
-             return;
-            }
-
-           kdown(node.left,k-1,blocker,ans);
-           kdown(node.right,k-1,blocker,ans);
-       }
     
-    public int distanceK_(TreeNode root,TreeNode target,int k,List<Integer>ans){
-            if(root==null)return -1;
-
-            if(root==target){
-              kdown(root,k-0,null,ans);
-              return 1;
-            }
-
-            int ld=distanceK_(root.left,target,k,ans);
-            if(ld!=-1){
-              kdown(root,k-ld,root.left,ans);
-              return ld+1;
-            }
-
-
-            int rd=distanceK_(root.right,target,k,ans);
-            if(rd!=-1){
-              kdown(root,k-rd,root.right,ans);
-              return rd+1;
-            }
-
-            return -1;
-         }
+    public void printkdistance(TreeNode root,int k,TreeNode blocker,List<Integer>ans){
+        if(root==null || k<0 || root==blocker)return;
+        
+        if(k==0){
+            ans.add(root.val);
+            return;
+        }
+        
+        printkdistance(root.left,k-1,blocker,ans);
+        printkdistance(root.right,k-1,blocker,ans);
+        
+    }
+    
+    public int distance_k(TreeNode root,TreeNode target,int k,List<Integer>ans){
+        if(root==null)return -1;
+        
+        if(root==target){
+            printkdistance(root,k,null,ans);
+            return 1;
+        }
+        
+        int ld=distance_k(root.left,target,k,ans);
+        if(ld!=-1){
+            printkdistance(root,k-ld,root.left,ans);
+            return ld+1;
+        }
+        
+        int rd=distance_k(root.right,target,k,ans);
+        if(rd!=-1){
+            printkdistance(root,k-rd,root.right,ans);
+            return rd+1;
+        }
+        
+        return -1;
+    }
     
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
         List<Integer>ans=new ArrayList<>();
-        distanceK_(root,target,k,ans);
+        distance_k(root,target,k,ans);
         return ans;
     }
 }
