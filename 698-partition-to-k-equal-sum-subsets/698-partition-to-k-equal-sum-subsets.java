96f8d89d5ef2e89809d05208c3400d4f339171b2
Harshit <harshitsaini3582@gmail.com>
2022-06-01 23:34:27 +0530

    Time: 1385 ms (40.19%), Space: 41.6 MB (63.12%) - LeetHub

---
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.java
@@ -0,0 +1,40 @@
+class Solution {
+    
+    public boolean canPartitionKSubsets_(int[]nums,int idx,int k,boolean[]vis,int tar,int sumSF){
+        if(k==0)return true;
+        
+        if(tar<sumSF)return false;
+        
+        if(sumSF==tar){
+            return canPartitionKSubsets_(nums,0,k-1,vis,tar,0);
+        }
+        
+        boolean res=false;
+        for(int i=idx;i<nums.length;i++){
+            if(vis[i])continue;
+            
+            vis[i]=true;
+            res=res||canPartitionKSubsets_(nums,i+1,k,vis,tar,sumSF+nums[i]);
+            vis[i]=false;
+        }
+        
+        return res;
+    }
+    
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int n=nums.length;
+        int sum=0;
+        int max=0;
+        
+        for(int val:nums){
+            sum+=val;
+            max=Math.max(max,val);
+        }
+        
+        if(sum%k!=0 || max>sum/k)return false;
+        boolean[]vis=new boolean[n];
+        
+        return canPartitionKSubsets_(nums,0,k,vis,sum/k,0);
+        
+    }
+}
