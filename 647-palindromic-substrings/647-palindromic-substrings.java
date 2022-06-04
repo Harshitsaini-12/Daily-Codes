0eed1b2396605d63b022db316ccf07549970d96e
Harshit <harshitsaini3582@gmail.com>
2022-06-04 11:05:41 +0530

    Time: 17 ms (37.64%), Space: 42.9 MB (36.37%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -10,7 +10,7 @@ class Solution {
                 else if(gap==1){
                     dp[i][j]=s.charAt(i)==s.charAt(j);
                 }else{
-                    if(s.charAt(i)==s.charAt(j) && dp[i+1][j-1]==true){
+                    if(s.charAt(i)==s.charAt(j) &&  dp[i+1][j-1]){
                         dp[i][j]=true;
                     }else{
                         dp[i][j]=false;
@@ -23,3 +23,20 @@ class Solution {
         return count++;
     }
 }
+
+// for printing the path
+
+// public String lis_backEng(String s,int si,int ei,int[][]dp){
+    
+//     if(si>=ei){
+//         return (ei==si?s.charAt(si)+"":"")
+//     }
+    
+//     if(s.charAt(si)==s.charAt(ei)){
+//         return s.charAt(si)+lis_backEng(s,si+1,ei-1,dp)+s.charAt(ei);
+//     }else if(dp[si][ei-1]>dp[si+1][ei]){
+//         return lis_backEng(s,si,ei-1,dp);
+//     }else{
+//         return lis_backEng(s,si+1,ei,dp);
+//     }
+// }
