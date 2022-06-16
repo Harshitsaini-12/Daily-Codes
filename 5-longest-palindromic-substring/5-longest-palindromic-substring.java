db71f791f2e8a74fd14021b4ca8612299a90d04d
Harshit <harshitsaini3582@gmail.com>
2022-06-16 11:40:25 +0530

    Time: 478 ms (19.91%), Space: 51.8 MB (40.56%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,36 +1,31 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int n=s.length();
-        int[][]dp=new int[n][n];
+        int[][]dp=new int[s.length()+1][s.length()+1];
         
-        int ans=0;
-        int sindex=0;
-        
-        //if we want to calculate total no of substring Palindromic then -->
+        int si=0;
         int count=0;
+        int ans=0;
         
-        for(int gap=0;gap<n;gap++){
-            for(int i=0,j=gap;j<n;i++,j++){
-                
+        for(int gap=0;gap<s.length();gap++){
+            for(int i=0,j=gap;j<s.length();j++,i++){
                 if(gap==0){
                     dp[i][j]=1;
                 }else if(gap==1 && s.charAt(i)==s.charAt(j)){
                     dp[i][j]=2;
                 }else{
-                    dp[i][j]=s.charAt(i)==s.charAt(j) && dp[i+1][j-1]>0 ? dp[i+1][j-1] + 2 : 0;
-                }
-                
-                if(dp[i][j]>ans){
-                    ans=dp[i][j];
-                    sindex=i;
+                    dp[i][j]=s.charAt(i)==s.charAt(j) && dp[i+1][j-1]>0?dp[i+1][j-1]+2:0;
                 }
                 
                 count+=dp[i][j]>0?1:0;
+                 if(dp[i][j]>ans){
+                    ans=dp[i][j];
+                    si=i;
+                 }
             }
         }
         
-          // System.out.print(count+" ");
-        return s.substring(sindex,sindex+ans);
-      
+        
+        return (s.substring(si,si+ans));
+        
     }
 }
