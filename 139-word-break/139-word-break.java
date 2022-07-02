6b62982c2bd90ece974e119f924f80101c971faa
Harshit <harshitsaini3582@gmail.com>
2022-07-02 21:52:55 +0530

    Time: 2 ms (97.31%), Space: 42.8 MB (72.65%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,31 +1,27 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        HashSet<String>set=new HashSet<>();
+         HashSet<String>set=new HashSet<>();
         
-        int maxlen=0;
-        
-        for(String ss:wordDict){
+         int maxLen=0;
+         for(String ss:wordDict){
             set.add(ss);
-            maxlen=Math.max(maxlen,ss.length());
-        }
+             maxLen=Math.max(maxLen,ss.length());
+         }
         
-        int n=s.length();
-        
-        boolean[]dp=new boolean[n+1];
+        boolean[]dp=new boolean[s.length()+1];
         dp[0]=true;
         
-        for(int i=0;i<=n;i++){
+        for(int i=0;i<=s.length();i++){
             if(!dp[i])continue;
             
-            for(int l=1;l<=maxlen && i+l<=n;l++){
-                String substr=s.substring(i,i+l);
+            for(int l=1;l<=maxLen && i+l<=s.length();l++){
+                String substr=s.substring(i,l+i);
                 
                 if(set.contains(substr)){
-                    dp[i+l]=true;
+                    dp[l+i]=true;
                 }
             }
         }
-        
-        return dp[n];
+        return dp[s.length()];
     }
 }
