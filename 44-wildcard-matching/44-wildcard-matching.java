cbea7b1148e688f9dc18218806312552f733aa00
Harshit <harshitsaini3582@gmail.com>
2022-07-02 13:36:13 +0530

    Time: 32 ms (69.05%), Space: 54.7 MB (11.33%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.java
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -1,21 +1,21 @@
 class Solution {
     public boolean isMatch(String s, String p) {
         
-        p=removeStar(p);
+        p=removestar(p);
         int n=s.length();
         int m=p.length();
         
         int[][]dp=new int[n+1][m+1];
-        
         for(int[]d:dp)Arrays.fill(d,-1);
         
-       int ans=memo(s,p,n,m,dp);
+        int ans=isMatch_(s,p,dp,n,m);
         return ans==1;
     }
     
-    public int memo(String s,String p,int n,int m,int[][]dp){
+    public int isMatch_(String s,String p,int[][]dp,int n,int m){
+        
         if(n==0 || m==0){
-            if(n==0 && m==0)return dp[n][m]=1;//true
+            if(n==0 && m==0)return dp[n][m]=1;
             else if(m==1 && p.charAt(m-1)=='*'){
                 return dp[n][m]=1;
             }else{
@@ -29,37 +29,37 @@ class Solution {
         char ch2=p.charAt(m-1);
         
         if(ch1==ch2 || ch2=='?'){
-            return dp[n][m]=memo(s,p,n-1,m-1,dp);
+            return isMatch_(s,p,dp,n-1,m-1);
         }else if(ch2=='*'){
-            boolean res=false;
+            boolean ans=false;
             
-            res=res || memo(s,p,n-1,m,dp)==1; //if it is including as a sequence
-            res=res || memo(s,p,n,m-1,dp)==1; //if treated as a empty sequence
+            ans=ans || isMatch_(s,p,dp,n-1,m)==1;// if taken into consdireation into sequence
+            ans=ans || isMatch_(s,p,dp,n,m-1)==1;// if not taken into sequence
             
-            return dp[n][m]= res ? 1 : 0;
+            return dp[n][m]=ans?1:0;
             
         }else{
-            return dp[n][m]=0;
+            return dp[n][m]=0; 
         }
-    
     }
     
-    public String removeStar(String s){
-        StringBuilder sb=new StringBuilder();
+    public String removestar(String p){
         
-        if(s.length()==0)return s;
-        sb.append(s.charAt(0));
+        StringBuilder sb=new StringBuilder();
+         if(p.length()==0)return p;
         
         int i=1;
+        sb.append(p.charAt(0));
         
-        while(i<s.length()){
-            while(i<s.length() && sb.charAt(sb.length()-1)=='*' && s.charAt(i)=='*')i++;
+        while(i<p.length()){
             
-            if(i<s.length())sb.append(s.charAt(i));
+            while(i<p.length() && sb.charAt(sb.length()-1)=='*' && p.charAt(i)=='*')i++;
+            
+            if(p.length()>i)sb.append(p.charAt(i));
+            
             i++;
         }
         
         return sb.toString();
     }
-        
 }
