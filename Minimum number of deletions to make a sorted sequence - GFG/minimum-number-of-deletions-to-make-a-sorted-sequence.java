1fbd03232bc2e7cd6315ab04f84213c0501d23ef
Harshit <harshitsaini3582@gmail.com>
2022-07-03 12:19:37 +0530

    Added solution - LeetHub

--- Minimum number of deletions to make a sorted sequence - GFG/minimum-number-of-deletions-to-make-a-sorted-sequence.java
+++ Minimum number of deletions to make a sorted sequence - GFG/minimum-number-of-deletions-to-make-a-sorted-sequence.java
@@ -33,9 +33,8 @@ class Solution
 	   
 	   for(int i=0;i<n;i++){
 	       dp[i]=1;
-	       
 	       for(int j=i-1;j>=0;j--){
-	           if(arr[j]<arr[i]){
+	           if(arr[i]>arr[j]){
 	               dp[i]=Math.max(dp[i],dp[j]+1);
 	           }
 	       }
