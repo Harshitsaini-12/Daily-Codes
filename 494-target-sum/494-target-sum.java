477a6b8f254c85b5e40ad42bf69b290275638a08
Harshit <harshitsaini3582@gmail.com>
2022-07-10 13:10:47 +0530

    Time: 8 ms (84.01%), Space: 42.4 MB (57.14%) - LeetHub

---
+++ 494-target-sum/494-target-sum.java
@@ -0,0 +1,34 @@
+class Solution {
+    
+    public int targetSum(int[]nums,int tar,int[][]dp,int n,int sum){
+        if(n==0){
+            return dp[n][sum]=tar==sum?1:0;
+        }
+        
+        if(dp[n][sum]!=-1)return dp[n][sum];
+        
+        int count=0;
+        
+        count+=targetSum(nums,tar,dp,n-1,sum + nums[n-1]);
+        count+=targetSum(nums,tar,dp,n-1,sum - nums[n-1]);
+        
+        return dp[n][sum]=count;
+    }
+    
+    public int findTargetSumWays(int[] nums, int target) {
+        
+        int n=nums.length;
+        
+        int sum=0;
+        for(int elem:nums)sum+=elem;
+        
+        if(target>sum || target<-sum){
+            return 0;
+        }
+        
+        int[][]dp=new int[n+1][2*sum+1];
+        for(int[]d:dp)Arrays.fill(d,-1);
+        
+        return targetSum(nums,target+sum,dp,n,sum);
+    }
+}
