249470d1c5148993c8c974b74680b52b6055dd08
Harshit <harshitsaini3582@gmail.com>
2022-07-12 13:15:37 +0530

    Time: 1 ms (100.00%), Space: 42 MB (82.58%) - LeetHub

---
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
@@ -0,0 +1,37 @@
+class Solution {
+    int[]par;
+    public int findPar(int u){
+        return par[u]==u?u:(par[u]=findPar(par[u]));
+    }
+    
+    public boolean equationsPossible(String[] equation) {
+
+        par=new int[26];
+        
+        for(int i=0;i<26;i++)par[i]=i;
+        
+        for(String s:equation){
+            if(s.charAt(1)=='='){
+                int p1=findPar(s.charAt(0)-'a');
+                int p2=findPar(s.charAt(3)-'a');
+                
+                if(p1!=p2){
+                    par[p1]=p2;
+                }
+            }
+        }
+        
+        for(String s:equation){
+            if(s.charAt(1)=='!'){
+                int p1=findPar(s.charAt(0)-'a');
+                int p2=findPar(s.charAt(3)-'a');
+                
+                if(p1==p2){
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+}
