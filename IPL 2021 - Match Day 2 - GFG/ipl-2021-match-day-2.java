3c237b9ebcaa5f194b758b900a262a0b50b4510f
Harshit <harshitsaini3582@gmail.com>
2022-07-15 15:30:28 +0530

    Added solution - LeetHub

---
+++ IPL 2021 - Match Day 2 - GFG/ipl-2021-match-day-2.java
@@ -0,0 +1,67 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main {
+    public static void main(String args[]) {
+        // taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // taking total count of testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            // taking total number of elements
+            int n = sc.nextInt();
+
+            // taking size of subArrays
+            int k = sc.nextInt();
+
+            // Declaring and Intializing an array of size n
+            int arr[] = new int[n];
+
+            // adding all the elements to the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            // Calling the method max_of_subarrays of class solve
+            // and storing the result in an ArrayList
+            ArrayList<Integer> res = new Solution().max_of_subarrays(arr, n, k);
+
+            // printing the elements of the ArrayList
+            for (int i = 0; i < res.size(); i++)
+                System.out.print(res.get(i) + " ");
+            System.out.println();
+        }
+    }
+}// } Driver Code Ends
+
+
+class Solution {
+    static ArrayList<Integer> max_of_subarrays(int nums[], int n, int k) {
+        // Your code here
+         PriorityQueue<Integer>pq=new PriorityQueue<>((a,b)->{
+           return nums[b]-nums[a]; 
+        });
+        
+        ArrayList<Integer>ans=new ArrayList<>();
+        int idx=0;
+        
+        for(int i=0;i<n;i++){
+            while(pq.size()>0 && pq.peek()<=i-k){
+                pq.remove();
+            }
+            
+            pq.add(i);
+            
+            if(i>=k-1){
+                ans.add(nums[pq.peek()]);
+                idx++;
+            }
+        }
+        
+        return ans;
+    }
+}
