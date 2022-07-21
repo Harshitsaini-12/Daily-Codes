f0bbd8c4e70b39bfcaf003dc7dc9797618b1ffa2
Harshit <harshitsaini3582@gmail.com>
2022-07-21 21:19:33 +0530

    Time: 15 ms (28.29%), Space: 47.3 MB (78.97%) - LeetHub

---
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[][] diagonalSort(int[][] mat) {
+        HashMap<Integer,PriorityQueue<Integer>>hm=new HashMap<>();
+        
+        int n=mat.length;
+        int m=mat[0].length;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int gap=i-j;
+                
+                if(!hm.containsKey(gap)){
+                  hm.put(gap,new PriorityQueue<>());
+                }
+                
+                hm.get(gap).add(mat[i][j]);
+            }
+        }
+        
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int gap=i-j;
+                
+                mat[i][j]=hm.get(gap).remove();
+            }
+        }
+        
+        return mat;
+    }
+}
+
+
+// gap between the diagonals is same
