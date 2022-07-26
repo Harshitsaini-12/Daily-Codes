5c1d6150ce47a1e67c18b996461c6de6a411e2d0
Harshit <harshitsaini3582@gmail.com>
2022-07-26 14:13:33 +0530

    Time: 110 ms (13.49%), Space: 161 MB (7.15%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -9,18 +9,18 @@ public:
         for(int i=3;i<=n;i++){
             string t="";
             
-            s=s+'$';
+            s=s+"$";
             int c=1;
             
             for(int j=1;j<s.length();j++){
-               if(s[j]!=s[j-1]){
-                   t=t+to_string(c);
-                   t=t+s[j-1];
-                   
-                   c=1;
-               }else{
-                   c++;
-               }
+                if(s[j]!=s[j-1]){
+                    t=t+to_string(c);
+                    t=t+s[j-1];
+                    c=1;
+                    
+                }else{
+                    c++;                   
+                }
             }
             s=t;
         }
