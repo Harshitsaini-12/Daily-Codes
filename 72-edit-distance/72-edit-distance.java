f4124331c80d219217dcb939ca6decd51687e46d
Harshit <harshitsaini3582@gmail.com>
2022-08-29 20:24:53 +0530

    Time: 12 ms (23.31%), Space: 44.7 MB (73.10%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -1,23 +1,22 @@
 class Solution {
     public int minDistance_memo(String s1,String s2,int n,int m,int[][]dp){
         if(n==0 || m==0){
-            return dp[n][m]=n!=0?n:m;
+            return dp[n][m] = n != 0 ? n : m;
         }
         
         if(dp[n][m]!=0){
             return dp[n][m];
         }
         
-        int a=minDistance_memo(s1,s2,n-1,m-1,dp);
-        int b=minDistance_memo(s1,s2,n-1,m,dp);
-        int c=minDistance_memo(s1,s2,n,m-1,dp);
+        int a=minDistance_memo(s1,s2,n-1,m-1,dp);//replace
+        int b=minDistance_memo(s1,s2,n-1,m,dp);//delete
+        int c=minDistance_memo(s1,s2,n,m-1,dp);//insert
         
         if(s1.charAt(n-1)==s2.charAt(m-1)){
             return dp[n][m]=a;
         }else{
             return dp[n][m]=Math.min(a,Math.min(b,c))+1;
         }
-        
         
     }
     
@@ -26,7 +25,6 @@ class Solution {
         int m=word2.length();
         
         int[][]dp=new int[n+1][m+1];
-        // for(int[]d:dp)Arrays.fill(dp,-1);
         
         int ans=minDistance_memo(word1,word2,n,m,dp);
         return ans;
