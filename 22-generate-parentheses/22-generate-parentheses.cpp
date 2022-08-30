38315e2232c3dff415340744a2dda768c32a8b47
Harshit <harshitsaini3582@gmail.com>
2022-08-30 21:06:17 +0530

    Time: 6 ms (59.53%), Space: 15.8 MB (19.70%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    
+    void helper(string out,int open,int close,vector<string>&s){
+        if(open==0 && close==0){
+            s.push_back(out);
+            return;   
+        }
+        
+        if(open!=0){
+            string out1=out;
+            out1.push_back('(');
+            
+            helper(out1,open-1,close,s);
+        }
+        
+        if(close>open){
+            string out2=out;
+            out2.push_back(')');
+            
+            helper(out2,open,close-1,s);
+        }
+        
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        vector<string>s;
+        
+        int open=n,close=n;
+        string output="";
+        
+        helper(output,open,close,s);
+        return s;
+    }
+};
