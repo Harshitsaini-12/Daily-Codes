1489091542023c618426cb76cb2299bdf2f2b4e5
Harshit <harshitsaini3582@gmail.com>
2022-09-01 17:22:07 +0530

    Time: 5 ms (65.36%), Space: 42.7 MB (82.99%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
@@ -17,67 +17,66 @@ class Solution {
     public class Pair{
         TreeNode node=null;
         int hl=0;
-
+        
         Pair(TreeNode node,int hl){
             this.node=node;
             this.hl=hl;
         }
     }
     
-    public void width(TreeNode root,int level,int[]minmax){
-        if(root==null)return;
-
-        minmax[0]=Math.min(level,minmax[0]);
-        minmax[1]=Math.max(level,minmax[1]);
-
-        width(root.left,level-1,minmax);
-        width(root.right,level+1,minmax);
+    public void minMaxWidth(TreeNode node,int hl,int[]minmax){
+        if(node==null)return;
+        
+        minmax[0]=Math.min(minmax[0],hl);
+        minmax[1]=Math.max(minmax[1],hl);
+        
+        minMaxWidth(node.left,hl-1,minmax);
+        minMaxWidth(node.right,hl+1,minmax);
+        
     }
     
     public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>>ans=new ArrayList<>();
         
-        List<List<Integer>> res = new ArrayList<>();
+        int []minmax=new int[2];
+        
+        minMaxWidth(root,0,minmax);
+        int len=minmax[1]-minmax[0]+1;
+        
+        for(int i=0;i<len;i++)ans.add(new ArrayList<>());
         
         PriorityQueue<Pair>pq=new PriorityQueue<>((a,b)->{
             return a.node.val-b.node.val;
         });
-
+        
         PriorityQueue<Pair>cq=new PriorityQueue<>((a,b)->{
             return a.node.val-b.node.val;
         });
-
-        int[]minmax=new int[2];
-        width(root,0,minmax);
-
-        int len=minmax[1]-minmax[0]+1;
-
-        for(int i=0;i<len;i++)res.add(new ArrayList<>());
-
+        
+        
         pq.add(new Pair(root,Math.abs(minmax[0])));
-
+        
         while(pq.size()!=0){
             int size=pq.size();
-
+            
             while(size-->0){
                 Pair rem=pq.remove();
-
                 TreeNode node=rem.node;
                 int hl=rem.hl;
-
-                res.get(hl).add(node.val);
-
+                
+                ans.get(hl).add(node.val);
+                
                 if(node.left!=null)cq.add(new Pair(node.left,hl-1));
-
+                
                 if(node.right!=null)cq.add(new Pair(node.right,hl+1));
+                
             }
-
+            
             PriorityQueue<Pair>temp=pq;
             pq=cq;
             cq=temp;
         }
-        return res;
+        
+        return ans;
     }
 }
-
-//tc --> 0(nlogn)
-//sc --> 0(n)
