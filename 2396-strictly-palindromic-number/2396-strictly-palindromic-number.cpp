c367fc4bedad0091073469d538a918586e6ae8b2
Harshit <harshitsaini3582@gmail.com>
2022-09-04 13:28:48 +0530

    Time: 5 ms (100.00%), Space: 5.9 MB (100.00%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    
+    bool isPalin(string &s){
+        int i=0;
+        int j=s.size()-1;
+        
+        while(i<=j){
+            if(s[i]!=s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+    
+    bool isStrictlyPalindromic(int n) {
+       for(int i=2;i<=n-2;i++){
+           string s;
+           int x=n;
+           
+           while(x!=0){
+               s.push_back(x%i);
+               x/=i;
+           }
+           
+           if(!isPalin(s)){
+               return false;
+           }
+       }
+        
+        return true;
+    }
+
+};
