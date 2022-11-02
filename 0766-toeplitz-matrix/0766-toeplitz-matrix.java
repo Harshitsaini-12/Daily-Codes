44b7139b3662a7d90794e3d98a2b9fb2cbffeffe
Harshit <harshitsaini3582@gmail.com>
2022-11-02 09:28:01 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (96.51%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -3,7 +3,6 @@ class Solution {
         int n=matrix.length;
         int m=matrix[0].length;
         
-        
         for(int i=1;i<n;i++){
             for(int j=1;j<m;j++){
                 if(matrix[i][j]!=matrix[i-1][j-1])return false;
